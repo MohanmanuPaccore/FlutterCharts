325a89e515c792dce2e8f60ba42deaf18043b754
PACLAP-MAC2 <paclap-mac2@PACLAP-MAC2s-MacBook-Pro.local>
2023-10-20 18:21:53 +0530

    shown y axis labels

--- custombarcharts/lib/main.dart
+++ custombarcharts/lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'dart:math';
 
+import 'package:custombarcharts/bar_chart.dart';
 import 'package:custombarcharts/chart_painter.dart';
 import 'package:flutter/material.dart';
 
@@ -107,6 +108,26 @@ class CustomBarChart extends StatelessWidget {
       builder: (context, constraints) {
         final chartWidth = max(60.0 * data.length, constraints.maxWidth); // Ensure a minimum bar width of 50
         final chartHeight = constraints.maxHeight;
+
+        return Center(
+          child: BarChart(
+            bars: [
+              Bar(label: 'A', value: 100),
+              Bar(label: 'B', value: 75),
+              Bar(label: 'C', value: 150),
+              Bar(label: 'D', value: 200),
+              Bar(label: 'E', value: 50),
+              Bar(label: 'F', value: 175),
+              Bar(label: 'G', value: 175),
+              Bar(label: 'H', value: 175),
+              Bar(label: 'I', value: 175),
+              Bar(label: 'J', value: 175),
+              Bar(label: 'K', value: 175),
+              Bar(label: 'L', value: 175),
+              Bar(label: 'M', value: 175),
+            ],
+          ),
+        );
 
         return SingleChildScrollView(
           scrollDirection: Axis.horizontal,
